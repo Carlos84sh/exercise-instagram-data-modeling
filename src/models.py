90082ec6ca1b5025d44728f1167542cb2f0d9705
Carlos84sh <Cs.huelves@gmail.com>
2020-12-13 03:26:06 +0000

    subida de ejercicio

--- src/models.py
+++ src/models.py
@@ -8,23 +8,45 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+class User(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+    
     id = Column(Integer, primary_key=True)
+    email = Column(String(120), nullable=False)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    user_name = Column(String(20),unique=True)
+    dob = Column(db.DateTime)
+    telephone = Column(Integer)
+    gender = Column(String(10),nullable=False)
 
-class Address(Base):
+
+class Post(Base):
     __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+   
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_name = db.Column(db.Integer, db.ForeignKey('User.user_name'))
+    comments = Column(String(250))
+    mp4 = Column(String(250))
+    post_date = Column(db.DateTime)
+    publisher_ID = db.Column(db.Integer, db.ForeignKey('User.id'))
+    
+
+class Comments(Base):
+    __tablename__ = 'address'
+   
+    id = Column(Integer, primary_key=True)
+    user_name = db.Column(db.Integer, db.ForeignKey('Post.user_name'))
+    comments = db.Column(db.Integer, db.ForeignKey('Post.comments'))
+    comment_date = Column(db.DateTime)
+
+
+class Followe(Base):
+    __tablename__ = 'address'
+   
+    id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    user_name = Column(String(20))
+    
 
     def to_dict(self):
         return {}
