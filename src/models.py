688ac970bcefb22326b1ae6925b0998a3c8a11ab
Carlos84sh <Cs.huelves@gmail.com>
2021-02-28 08:10:25 +0000

    ejercicio finquitado

--- src/models.py
+++ src/models.py
@@ -9,47 +9,54 @@ from eralchemy import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'person'
-    
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    email = Column(String(120), nullable=False)
-    name = Column(String(250), nullable=False)
-    last_name = Column(String(100), nullable=False)
-    user_name = Column(String(20),unique=True)
-    dob = Column(db.DateTime)
-    telephone = Column(Integer)
-    gender = Column(String(10),nullable=False)
+    first_name = Column(String(50)) # modificado
+    last_name = Column(String(50))
+    nickname = Column(String(20),unique=True) # modificado
+    email = Column(String(120), nullable=False, unique= True)
+    password = Column(String(20), nullable=False)
+    #gender = Column(String(10),nullable=False)
+    #dob = Column(db.DateTime)
+    #telephone = Column(Integer)
 
 
 class Post(Base):
-    __tablename__ = 'address'
+    __tablename__ = 'post'
    
     id = Column(Integer, primary_key=True)
-    user_name = db.Column(db.Integer, db.ForeignKey('User.user_name'))
-    comments = Column(String(250))
-    mp4 = Column(String(250))
-    post_date = Column(db.DateTime)
-    publisher_ID = db.Column(db.Integer, db.ForeignKey('User.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comments = Column(String(1000), nullable=False)
+    #publisher_ID = Column(Integer, ForeignKey('user.id')) REVISAR PORQUE NO FUNCIONA
+    image = Column(String(400), nullable=False)
     
 
 class Comments(Base):
-    __tablename__ = 'address'
+    __tablename__ = 'comments'
    
     id = Column(Integer, primary_key=True)
-    user_name = db.Column(db.Integer, db.ForeignKey('Post.user_name'))
-    comments = db.Column(db.Integer, db.ForeignKey('Post.comments'))
-    comment_date = Column(db.DateTime)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    text = Column(String(4000), nullable=False)
 
 
-class Followe(Base):
-    __tablename__ = 'address'
+class Followers(Base):
+    __tablename__ = 'followers'
    
-    id = db.Column(db.Integer, db.ForeignKey('User.id'))
-    user_name = Column(String(20))
+    id = Column(Integer, primary_key=True)
+    user_from = Column(Integer, ForeignKey('user.id'))
+    user_to = Column(Integer, ForeignKey('user.id'))
+
+class Likes(Base):
+    __tablename__ = 'likes'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
     
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+render_er(Base, 'diagram2.png')
